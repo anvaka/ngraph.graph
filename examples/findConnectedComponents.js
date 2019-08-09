4fa3491ebca10d3842b53f9af42d311e964d9e22
Andriy Kashcha <anvaka@gmail.com>
2019-08-08 21:41:42 -0700

    example for connected components

---
+++ examples/findConnectedComponents.js
@@ -0,0 +1,49 @@
+/**
+ * This examples shows how to find all connected components of a graph.
+ * 
+ * Created in response to https://github.com/anvaka/ngraph.graph/issues/18
+ */
+module.exports = function findConnectedComponents(graph) {
+  var nodeIdToComponentId = new Map();
+
+  var connectedComponents = [];
+  var lastComponentId = 0;
+
+  graph.forEachNode(function(node) {
+    if (nodeIdToComponentId.has(node.id)) {
+      // we already seen this cluster. Ignore it.
+      return;
+    }
+
+    // We found a new connected component:
+    nodeIdToComponentId.set(node.id, lastComponentId);
+    var currentComponent = [node.id];
+    connectedComponents.push(currentComponent);
+
+    // Let's find what other nodes belong to this component
+    dfs(graph, node.id, otherNode => {
+      let componentId = nodeIdToComponentId.get(otherNode.id)
+      if (componentId !== undefined && componentId === lastComponentId) {
+        // this is possible when we have a loop. Just ignore the node.
+        return false;
+      } else if (componentId !== undefined) {
+        throw new Error('Reached a component from another component. DFS is broken?');
+      }
+
+      currentComponent.push(otherNode.id);
+      nodeIdToComponentId.set(otherNode.id, lastComponentId);
+
+      return true; // let's visit neighbors
+    });
+
+    lastComponentId += 1;
+  });
+
+  return connectedComponents;
+}
+
+function dfs(graph, startFromNodeId, visitor) {
+  graph.forEachLinkedNode(startFromNodeId, function(otherNode) {
+    if (visitor(otherNode)) dfs(graph, otherNode.id, visitor);
+  });
+}
---
+++ test/findConnectedComponents.js
@@ -0,0 +1,25 @@
+var test = require('tap').test;
+var findConnectedComponents = require('../examples/findConnectedComponents');
+var createGraph = require('..');
+
+test('can find connected components', function(t) {
+  // our graph has three components
+  var graph = createGraph();
+  graph.addLink(1, 2);
+  graph.addLink(2, 3);
+
+  graph.addLink(5, 6);
+  graph.addNode(8);
+  // let's add loop:
+  graph.addLink(9, 9);
+
+  // lets verify it:
+  var components = findConnectedComponents(graph);
+  t.equal(components.length, 4, 'all components found');
+  t.deepEqual(components[0], [1, 2, 3], 'first component found')
+  t.deepEqual(components[1], [5, 6], 'second component found')
+  t.deepEqual(components[2], [8], 'third component found')
+  t.deepEqual(components[3], [9], 'fourth component found')
+
+  t.end();
+});
