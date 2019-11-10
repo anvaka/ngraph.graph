f8c15917d15d8f699f5b7879742fc50e754b1aea
Andriy Kashcha <anvaka@gmail.com>
2019-11-09 22:12:51 -0800

    update iteration test

--- perf/node-iteration.js
+++ perf/node-iteration.js
@@ -5,39 +5,26 @@ var createGraph = require('../'),
 
 var suite = new Benchmark.Suite();
 
-var graphWithOutMap = createGraph();
-var graphWithMap = createGraph({useMap: true});
+var graph = createGraph();
 for (var i = 0; i < numberOfNodes; ++i) {
-  graphWithOutMap.addNode('hello' + i, i);
-  graphWithMap.addNode('hello' + i, i);
+  graph.addNode('hello' + i, i);
 }
 
-var sum1 = 0;
-var sum2 = 0;
+var sum = 0;
 
 // add tests
-suite.add('graphWithOutMap', function() {
+suite.add('forEachNode', function() {
   var localSum = 0;
-  graphWithOutMap.forEachNode(function (node) {
+  graph.forEachNode(function (node) {
     localSum += node.data;
   });
-  sum1 = localSum;
-})
-.add('graphWithMap', function() {
-  var localSum = 0;
-  graphWithMap.forEachNode(function (node) {
-    localSum += node.data;
-  });
-  sum2 = localSum;
+  sum = localSum;
 })
 .on('cycle', function(event) {
   console.log(String(event.target));
 })
 .on('complete', function() {
-   if (sum1 !== sum2) {
-     throw new Error('Invalid test')
-   }
-
+   console.log(sum);
    console.log('Fastest is ' + this.filter('fastest').map("name"));
 })
 // run async
